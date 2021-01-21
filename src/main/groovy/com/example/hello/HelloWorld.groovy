6d32d94cd6198b3c6fccfb2cbf2af780483f8fa2
suman <suman@flexrentalsolutions.com>
2021-01-20 22:59:51 -0500

    travis coverall

---
+++ src/main/groovy/com/example/hello/HelloWorld.groovy
@@ -0,0 +1,11 @@
+package com.example.hello
+
+class HelloWorld {
+
+    HelloWorld(){}
+    
+    String sayHello(String name) {
+        return "Hello $name"
+    }
+    
+}
---
+++ src/test/groovy/com/example/hello/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package com.example.hello;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+    HelloWorld tested = new HelloWorld();
+    
+    @Test
+    void sayHello() {
+        Assertions.assertEquals("Hello Suman", tested.sayHello("Suman"));
+    }
+}
